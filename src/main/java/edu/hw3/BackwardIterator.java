b43b9141377eeddfd3bf0d89aab1fa8214611f85
Vasilii Lastin <vasya.lastin76@gmail.com>
2023-10-21 23:08:42 +0300

    Implemented BackwardIterator task

---
+++ src/main/java/edu/hw3/BackwardIterator.java
@@ -0,0 +1,23 @@
+package edu.hw3;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class BackwardIterator <T> implements Iterator<T> {
+    private final Iterator<T> backwardIterator;
+
+    BackwardIterator(List<T> list) {
+        this.backwardIterator = list.reversed().iterator();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return backwardIterator.hasNext();
+    }
+
+    @Override
+    public T next() {
+        return backwardIterator.next();
+    }
+}
