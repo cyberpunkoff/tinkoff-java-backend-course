aea0b6d463dec6552acfcd74c911440ef804b0a6
Vasilii Lastin <vasya.lastin76@gmail.com>
2023-11-12 09:36:28 +0300

    Add third task

---
+++ src/main/java/edu/hw6/task3/filters/FileSizeFilter.java
@@ -0,0 +1,22 @@
+package edu.hw6.task3.filters;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public abstract class FileSizeFilter implements AbstractFilter {
+    public static FileSizeFilter largerThan(long sizeInBytes) {
+        return new FileSizeFilter() {
+            @Override
+            public boolean accept(Path path) {
+                // блинб.. как бы тут исключение пробросить..
+                try {
+                    long bytes = Files.size(path);
+                    return bytes  > sizeInBytes;
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+    }
+}
