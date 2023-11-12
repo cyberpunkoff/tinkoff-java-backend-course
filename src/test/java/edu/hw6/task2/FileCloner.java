12c4ef5759e6e0178ce2aad8929a681f5e304e23
Vasilii Lastin <vasya.lastin76@gmail.com>
2023-11-12 07:52:39 +0300

    Add FileCloner

---
+++ src/test/java/edu/hw6/task2/FileCloner.java
@@ -0,0 +1,50 @@
+package edu.hw6.task2;
+
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.apache.commons.io.FilenameUtils;
+
+public class FileCloner {
+    private static final String FIRST_COPY_PATTERN = "%s — копия";
+    private static final String NEXT_COPY_PATTERN = "%s — копия (%d)";
+
+    public static void clonePath(Path path) {
+        Path originalPath = path.toAbsolutePath();
+        int copyCount = 1;
+        boolean copyMade = false;
+
+        while (!copyMade) {
+            try {
+                String newFileName = generateNewFilename(originalPath.getFileName().toString(), copyCount);
+                Path copy = originalPath.getParent().resolve(newFileName);
+                Files.copy(originalPath, copy);
+                copyMade = true;
+            } catch (FileAlreadyExistsException e) {
+                copyCount++;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private static String generateNewFilename(String oldName, int copyCount) {
+        String extension = FilenameUtils.getExtension(oldName);
+        String fileName = FilenameUtils.getName(oldName);
+        StringBuilder result = new StringBuilder();
+
+        if (copyCount == 1) {
+            result.append(String.format(FIRST_COPY_PATTERN, fileName));
+        } else {
+            result.append(String.format(NEXT_COPY_PATTERN, fileName, copyCount));
+        }
+
+        if (!extension.isEmpty()) {
+            result.append('.');
+            result.append(extension);
+        }
+
+        return result.toString();
+    }
+}
