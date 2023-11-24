256c3e9d5036ec9fce5002d7e7ded31da2daccd6
Vasilii Lastin <vasya.lastin76@gmail.com>
2023-11-25 01:05:43 +0300

    Add third and a half task

---
+++ src/test/java/edu/hw7/task3/PersonDatabaseReadWriteLockTest.java
@@ -0,0 +1,39 @@
+package edu.hw7.task3;
+
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PersonDatabaseReadWriteLockTest {
+    @Test
+    void testReadWriteLock() {
+        List<Person> personList = List.of(
+            new Person(1, "Maks", "hello", "+123"),
+            new Person(2, "Maks2", "hello2", "+1234"),
+            new Person(3, "Maks3", "hello3", "+1235")
+        );
+        PersonDatabase personDatabase = new PersonDatabaseReadWriteLock();
+
+        Thread first = new Thread(() -> personList.forEach(personDatabase::add));
+        Thread second = new Thread(() -> personList.forEach(
+            person -> {
+                    List<Person> foundByName = personDatabase.findByName(person.name());
+                    List<Person> foundByAddress = personDatabase.findByAddress(person.address());
+                    List<Person> foundByPhone = personDatabase.findByPhone(person.phoneNumber());
+
+                    assertThat(foundByName).containsExactlyInAnyOrderElementsOf(foundByAddress);
+                    assertThat(foundByAddress).containsExactlyInAnyOrderElementsOf(foundByPhone);
+            }
+        ));
+
+        first.start();
+        second.start();
+
+        try {
+            first.join();
+            second.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
