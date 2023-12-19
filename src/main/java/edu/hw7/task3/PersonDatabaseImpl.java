62f34cd09de9bc1d47bdf64bb2418d3dccb304c8
Vasilii Lastin <vasya.lastin76@gmail.com>
2023-12-19 20:03:51 +0300

    feat: change PersonDatabase structure

--- src/main/java/edu/hw7/task3/PersonDatabaseImpl.java
+++ src/main/java/edu/hw7/task3/PersonDatabaseImpl.java
@@ -1,52 +1,30 @@
 package edu.hw7.task3;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
-public class PersonDatabaseImpl implements PersonDatabase {
-    private final Map<Integer, Person> persons;
-    private final Map<String, List<Person>> nameCache;
-    private final Map<String, List<Person>> addressCache;
-    private final Map<String, List<Person>> phoneCache;
-
-    public PersonDatabaseImpl() {
-        persons = new HashMap<>();
-        nameCache = new HashMap<>();
-        addressCache = new HashMap<>();
-        phoneCache = new HashMap<>();
-    }
-
+public class PersonDatabaseImpl extends AbstractPersonDatabase {
     @Override
     public synchronized void add(Person person) {
-        persons.put(person.id(), person);
-        nameCache.computeIfAbsent(person.name(), k -> new ArrayList<>()).add(person);
-        addressCache.computeIfAbsent(person.address(), k -> new ArrayList<>()).add(person);
-        phoneCache.computeIfAbsent(person.phoneNumber(), k -> new ArrayList<>()).add(person);
+        super.add(person);
     }
 
     @Override
     public synchronized void delete(int id) {
-        Person personToDelete = persons.get(id);
-        nameCache.get(personToDelete.name()).remove(personToDelete);
-        addressCache.get(personToDelete.address()).remove(personToDelete);
-        phoneCache.get(personToDelete.phoneNumber()).remove(personToDelete);
+        super.delete(id);
     }
 
     @Override
     public List<Person> findByName(String name) {
-        return Optional.ofNullable(nameCache.get(name)).orElse(List.of());
+        return super.findByName(name);
     }
 
     @Override
     public List<Person> findByAddress(String address) {
-        return Optional.ofNullable(addressCache.get(address)).orElse(List.of());
+        return super.findByAddress(address);
     }
 
     @Override
     public List<Person> findByPhone(String phone) {
-        return Optional.ofNullable(phoneCache.get(phone)).orElse(List.of());
+        return super.findByPhone(phone);
     }
 }
--- src/main/java/edu/hw7/task3/PersonDatabaseReadWriteLock.java
+++ src/main/java/edu/hw7/task3/PersonDatabaseReadWriteLock.java
@@ -1,68 +1,53 @@
 package edu.hw7.task3;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
-public class PersonDatabaseReadWriteLock implements PersonDatabase {
+public class PersonDatabaseReadWriteLock extends AbstractPersonDatabase {
     private final ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
-    private final Map<Integer, Person> persons;
-    private final Map<String, List<Person>> nameCache;
-    private final Map<String, List<Person>> addressCache;
-    private final Map<String, List<Person>> phoneCache;
-
-    public PersonDatabaseReadWriteLock() {
-        persons = new HashMap<>();
-        nameCache = new HashMap<>();
-        addressCache = new HashMap<>();
-        phoneCache = new HashMap<>();
-    }
 
     @Override
     public void add(Person person) {
         readWriteLock.writeLock().lock();
-        persons.put(person.id(), person);
-        nameCache.computeIfAbsent(person.name(), k -> new ArrayList<>()).add(person);
-        addressCache.computeIfAbsent(person.address(), k -> new ArrayList<>()).add(person);
-        phoneCache.computeIfAbsent(person.phoneNumber(), k -> new ArrayList<>()).add(person);
+        super.add(person);
         readWriteLock.writeLock().unlock();
     }
 
     @Override
     public void delete(int id) {
         readWriteLock.writeLock().lock();
-        Person personToDelete = persons.get(id);
-        nameCache.get(personToDelete.name()).remove(personToDelete);
-        addressCache.get(personToDelete.address()).remove(personToDelete);
-        phoneCache.get(personToDelete.phoneNumber()).remove(personToDelete);
+        super.delete(id);
         readWriteLock.writeLock().unlock();
     }
 
     @Override
     public List<Person> findByName(String name) {
-        readWriteLock.readLock().lock();
-        List<Person> result = Optional.ofNullable(nameCache.get(name)).orElse(List.of());
-        readWriteLock.readLock().unlock();
-        return result;
+        try {
+            readWriteLock.readLock().lock();
+            return super.findByName(name);
+        } finally {
+            readWriteLock.readLock().unlock();
+        }
     }
 
     @Override
     public List<Person> findByAddress(String address) {
-        readWriteLock.readLock().lock();
-        List<Person> result = Optional.ofNullable(addressCache.get(address)).orElse(List.of());
-        readWriteLock.readLock().unlock();
-        return result;
+        try {
+            readWriteLock.readLock().lock();
+            return super.findByAddress(address);
+        } finally {
+            readWriteLock.readLock().unlock();
+        }
     }
 
     @Override
     public List<Person> findByPhone(String phone) {
-        readWriteLock.readLock().lock();
-        List<Person> result = Optional.ofNullable(phoneCache.get(phone)).orElse(List.of());
-        readWriteLock.readLock().unlock();
-        return result;
+        try {
+            readWriteLock.readLock().lock();
+            return super.findByPhone(phone);
+        } finally {
+            readWriteLock.readLock().unlock();
+        }
     }
 }
